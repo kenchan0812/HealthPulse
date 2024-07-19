9e1ce65d3e443b90a011672cb5121f92459c93bf
Kenji Kotani <kenji.kotani944708@gmail.com>
2024-07-19 23:40:06 +0800

    integrated appwrite to the form

--- src/components/forms/PatientForm.tsx
+++ src/components/forms/PatientForm.tsx
@@ -10,6 +10,7 @@ import SubmitButton from "../SubmitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
+import { createUser } from "@/lib/actions/patient.actions";
 
 export enum FormFieldType {
   INPUT = "input",
@@ -33,24 +34,25 @@ const PatientForm = () => {
     },
   });
 
-  async function onSubmit({
+  const onSubmit = async ({
     name,
     email,
     phone,
-  }: z.infer<typeof UserFormValidation>) {
+  }: z.infer<typeof UserFormValidation>) => {
     setIsLoading(true);
     try {
-      // const userData = {
-      //   name,
-      //   email,
-      //   phone,
-      // };
-      // const user = await createUser(userData);
-      // if (user) router.push(`/patient/${user.$id}/register`);
+      const userData = {
+        name,
+        email,
+        phone,
+      };
+      const user = await createUser(userData);
+      console.log({ user });
+      if (user) router.push(`/patient/${user.$id}/register`);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
